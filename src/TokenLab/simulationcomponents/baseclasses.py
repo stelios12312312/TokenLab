3f25e05e89946997633d670968ba1c0e03102807
michellew1989 <wmjpku@gmail.com>
2024-08-03 11:50:59 +0100

    reformat source PEP8

--- src/TokenLab/simulationcomponents/baseclasses.py
+++ src/TokenLab/simulationcomponents/baseclasses.py
@@ -5,155 +5,168 @@ Created on Fri Nov 18 12:17:07 2022
 
 @author: stylianoskampakis
 """
-from typing import List, Dict,Union
+from typing import List, Dict, Union
 import numpy as np
 import pandas as pd
 
-        
 
-class AddOn():
+class AddOn:
     """
     Class which is used within controllers to affect certain internal parameters (e.g. noise)
     """
-    
-    def __init__(self):
-        
-        pass
-    
-    
-    def apply(self)->float:
-        
-        pass
-    
-class Initialisable():
-    
-    def __init__(self):
-        self.initialised=False
-    
-    def initialise(self):
-        
-        self.initialised=True
-    
 
-class Controller():
-    
+    def __init__(self):
+
+        pass
+
+    def apply(self) -> float:
+
+        pass
+
+
+class Initialisable:
+
+    def __init__(self):
+        self.initialised = False
+
+    def initialise(self):
+
+        self.initialised = True
+
+
+class Controller:
     """
     Base class for all the sub-components of a simulation within a token economy
     """
-    
-    def __init__(self,name:str=None):
-        self.name=name
-        self.iteration=0
-        self.max_iteration=None
-        self.dependencies={}
+
+    def __init__(self, name: str = None):
+        self.name = name
+        self.iteration = 0
+        self.max_iteration = None
+        self.dependencies = {}
         pass
-    
+
     # def is_independent(self):
     #     return isinstance(usm,Independent)
-    
-    
-    def link(self,dependency_parent,dependency_instance)->bool:
-        
-        if hasattr(self,'dependencies'):
-            if isinstance(dependency_instance,dependency_parent):
-                self.dependencies[dependency_parent]= dependency_instance
+
+    def link(self, dependency_parent, dependency_instance) -> bool:
+
+        if hasattr(self, "dependencies"):
+            if isinstance(dependency_instance, dependency_parent):
+                self.dependencies[dependency_parent] = dependency_instance
             else:
-                raise Exception('Tried to link incompatible type.')
-        
+                raise Exception("Tried to link incompatible type.")
+
         return True
-    
-    def test_integrity(self)->bool:
+
+    def test_integrity(self) -> bool:
         """
         Tests whether all dependencies have a linked object or not.
-        
+
         The type of the link object is already checked in the link function.
         """
-        nones=0
-        if len(self.dependencies.keys())>0:
+        nones = 0
+        if len(self.dependencies.keys()) > 0:
             for k in self.dependencies.keys():
                 if self.dependencies[k] is None:
-                    nones+=1
-        if nones==len(self.dependencies.keys()):
+                    nones += 1
+        if nones == len(self.dependencies.keys()):
             return False
-        
+
         return True
-    
+
+    def test_integrity(self) -> bool:
+        """
+        Tests whether all dependencies have a linked object or not.
+
+        The type of the link object is already checked in the link function.
+        """
+        nones = sum(
+            1 for dependency in self.dependencies.values() if dependency is None
+        )
+        return nones != len(self.dependencies)
+
     def get_dependencies(self):
-        
-        print('Dependencies names are: ')
+
+        print("Dependencies names are: ")
         for d in self.dependencies.keys():
             try:
                 print(self.dependencies[d].name)
             except:
-                print('None')
-        
-        return self.dependencies
-    
-    
-    def __getitem__(self,item):
-        return getattr(self,item)
-    
-    
-class AgentPool(Controller):
-    
-    def __init__(self):
-        
-        self.num_users=0
-        self.transactions=0
-        self.iteration=0
-        self.currency=None
-        self.name=None
-        self.dependencies={TokenEconomy:None}
-        #This is a mechanism that enables agent pools to generate new pools
-        #The new pools need to change at the execute() function, and follow a format like
-        #[('AgentPool',object),('SupplyPool':object)]
-        self.new_pools = []
-        
-        
+                print("None")
 
-    def __print__(self)->str:
-        users=self.num_users
-        trans=self.transactions_controller.transactions_value
-        
-        return str(users)+'\n'+str(trans)
-    
-    def report(self)->Dict:
+        return self.dependencies
+
+    def __getitem__(self, item):
+        return getattr(self, item)
+
+
+class AgentPool(Controller):
+
+    def __init__(self):
+
+        self.num_users = 0
+        self.transactions = 0
+        self.iteration = 0
+        self.currency = None
+        self.name = None
+        self.dependencies = {TokenEconomy: None}
+        # This is a mechanism that enables agent pools to generate new pools
+        # The new pools need to change at the execute() function, and follow a format like
+        # [('AgentPool',object),('SupplyPool':object)]
+        self.new_pools = []
+
+    def __print__(self) -> str:
+        users = self.num_users
+        trans = self.transactions_controller.transactions_value
+
+        return str(users) + "\n" + str(trans)
+
+    def report(self) -> Dict:
         """
         Returns user and transaction data from the current iteration
         """
-        rep={'users':self.num_users,'transactions':self.transactions_controller.transactions_value}
-        
-        
-        return rep
-    
-    def get_transactions(self)->float:
-        
-        return self.transactions
-    
-    def get_num_users(self)->int:
-        
-        return self.num_users
-    
-    def reset(self)->None:
-        
-        self.iteration=0
-        
-    def execute(self)->list:
-        
-        return self.new_pools
-        
+        rep = {
+            "users": self.num_users,
+            "transactions": self.transactions_controller.transactions_value,
+        }
 
-class TokenEconomy():
+        return rep
+
+    def get_transactions(self) -> float:
+
+        return self.transactions
+
+    def get_num_users(self) -> int:
+
+        return self.num_users
+
+    def reset(self) -> None:
+
+        self.iteration = 0
+
+    def execute(self) -> list:
+
+        return self.new_pools
+
+
+class TokenEconomy:
     """
     Base class for the simulation
     """
-    
-    def __init__(self,
-                 holding_time:Union[float,Controller],supply:Union[float,Controller],
-                 fiat:str,token:str,
-                 unit_of_time:str,price_function:Controller,token_initial_price:List,adapt_supply_to_token_sales:bool=False,
-                 name:str=None)->None:
-        
+
+    def __init__(
+        self,
+        holding_time: Union[float, Controller],
+        supply: Union[float, Controller],
+        fiat: str,
+        token: str,
+        unit_of_time: str,
+        price_function: Controller,
+        token_initial_price: List,
+        adapt_supply_to_token_sales: bool = False,
+        name: str = None,
+    ) -> None:
         """
         fiat: the fiat currency used to denominate the economy
         tokens: a list of the token synmbols
@@ -162,69 +175,72 @@ class TokenEconomy():
         price_function: the price function used to simulate the price(e.g. equation of exchange)
         token_initial_price: the initial price for each token denominated in fiat
         """
-        self.fiat=fiat
-        self.token=token
-        self._price_function=price_function
-        
-        self.unit_of_time=unit_of_time
-        
-        self.adapt_supply_to_token_sales=adapt_supply_to_token_sales
+        self.fiat = fiat
+        self.token = token
+        self._price_function = price_function
 
-        self._holding_time_controller=holding_time
+        self.unit_of_time = unit_of_time
 
-        self._supply=supply
-        self._supply_pools=[]
-        self._supply_store=[]
-        
-        self._agent_pools=[]
-        
-        self._num_users_store=[]
-        self.num_users=0
+        self.adapt_supply_to_token_sales = adapt_supply_to_token_sales
 
-        
-        self._transactions_value_store_in_fiat=[]
-        
-        self._transactions_value_store_in_tokens={}
-        self.prices={}
+        self._holding_time_controller = holding_time
+
+        self._supply = supply
+        self._supply_pools = []
+        self._supply_store = []
+
+        self._agent_pools = []
+
+        self._num_users_store = []
+        self.num_users = 0
+
+        self._transactions_value_store_in_fiat = []
+
+        self._transactions_value_store_in_tokens = {}
+        self.prices = {}
         # for tok in tokens:
         #     self._transactions_value_store_in_tokens[tok]=[]
         #     self.prices[tok]=None
-        
-        self._transactions_value_store_in_tokens=[]
-        self.price=token_initial_price
-            
-        self._prices_store=[]
-        self.iteration=0
-        
-        self.transactions_volume_in_tokens=0
-        self.transactions_value_in_fiat=0
-        
-        self.holding_time=None    
-        self._holding_time_store=[]
-        
-        self._effective_holding_time_store=[]
-        
+
+        self._transactions_value_store_in_tokens = []
+        self.price = token_initial_price
+
+        self._prices_store = []
+        self.iteration = 0
+
+        self.transactions_volume_in_tokens = 0
+        self.transactions_value_in_fiat = 0
+
+        self.holding_time = None
+        self._holding_time_store = []
+
+        self._effective_holding_time_store = []
+
         self._treasury_store = []
-        
+
         self._num_transactions_store = []
-        
+
         self.name = name
-        
+
         return None
-    
+
     def execute(self):
-        
+
         pass
-    
-    def get_state(self)->Dict:
-        
-        state={'transactions_'+self.fiat:self.transactions_value_in_fiat,'supply':self.supply,
-               'holding_time':self.holding_time,'num_users':self.num_users}
-        
-        state['transactions_'+self.token]=self.transactions_volume_in_tokens
-        state[self.token+'_price']=self.price
-        
+
+    def get_state(self) -> Dict:
+
+        state = {
+            "transactions_" + self.fiat: self.transactions_value_in_fiat,
+            "supply": self.supply,
+            "holding_time": self.holding_time,
+            "num_users": self.num_users,
+        }
+
+        state["transactions_" + self.token] = self.transactions_volume_in_tokens
+        state[self.token + "_price"] = self.price
+
         return state
-    
-    def __getitem__(self,item):
-        return getattr(self,item)
+
+    def __getitem__(self, item):
+        return getattr(self, item)
--- src/TokenLab/simulationcomponents/usergrowthclasses.py
+++ src/TokenLab/simulationcomponents/usergrowthclasses.py
@@ -6,9 +6,9 @@ Created on Fri Nov 18 12:18:42 2022
 @author: stylianoskampakis
 """
 
-from typing import List, Dict,Union
+from typing import List, Dict, Union
 from baseclasses import *
-from TokenLab.utils.helpers import log_saturated_space,logistic_saturated_space
+from TokenLab.utils.helpers import log_saturated_space, logistic_saturated_space
 import copy
 import scipy
 import warnings
@@ -20,270 +20,278 @@ class UserGrowth(Controller):
     """
     Base class that models user growth. Dependencies include AgentPool and TokenEconomy.
     """
-    
+
     def __init__(self):
-        super(UserGrowth,self).__init__()
-        self.num_users=0
-        self._num_users_store=None
-        
-        #conditional dependence, it is used only in conditions
-        self.dependencies={AgentPool:None,TokenEconomy:None}
-        
+        super(UserGrowth, self).__init__()
+        self.num_users = 0
+        self._num_users_store = None
+
+        # conditional dependence, it is used only in conditions
+        self.dependencies = {AgentPool: None, TokenEconomy: None}
+
         return None
-    
-    
-    def execute(self)->int:        
+
+    def execute(self) -> int:
         if self._num_users_store is not None:
-            #this is only for precomputed user populations, like UserGrowth_Spaced
-            self.num_users=self._num_users_store[self.iteration]
+            # this is only for precomputed user populations, like UserGrowth_Spaced
+            self.num_users = self._num_users_store[self.iteration]
         self.iteration = self.iteration + 1
 
         return self.num_users
-    
-    
-    def get_num_users(self)->int:
-        
+
+    def get_num_users(self) -> int:
+
         return self.num_users
-    
-    def reset(self)->None:
+
+    def reset(self) -> None:
         """
         Resets the iteration counter to 0.
 
         """
-        self.iteration=0
-    
-    
-    
+        self.iteration = 0
+
+
 class UserGrowth_Spaced(UserGrowth):
-    
     """
     Creates the number of users (for a predetermined number of iterations)
     using a function that is spacing out users between min and max. Options include:
-    
+
     linearspace, logspace, geomspace, logistic
-    
+
     The class pre-calculates all users internally and stores them in _num_users_store.
-    
+
     When executed it simply returns the users at the current iteration of its lifecycle.
-    
+
     """
-    
-    
-    def __init__(self,initial_users:int,max_users:int,num_steps:int,
-                 space_function:Union[np.linspace,np.logspace,np.geomspace,log_saturated_space,logistic_saturated_space]=np.linspace,name:str=None,
-                 noise_addons:[AddOn]=None,use_difference:bool=False)->None:
+
+    def __init__(
+        self,
+        initial_users: int,
+        max_users: int,
+        num_steps: int,
+        space_function: Union[
+            np.linspace,
+            np.logspace,
+            np.geomspace,
+            log_saturated_space,
+            logistic_saturated_space,
+        ] = np.linspace,
+        name: str = None,
+        noise_addons: [AddOn] = None,
+        use_difference: bool = False,
+    ) -> None:
         """
-        use_difference: If True, it will calculate and return the difference between time steps. Essentially, the user 
+        use_difference: If True, it will calculate and return the difference between time steps. Essentially, the user
         base will grow according to the difference between the steps in the sequence.
-        
+
         Otherwise, it returns the actual value.
-        
+
         For example:
             initial_users=100
             growth=[100,120,150,200]
             final=[100,120,150,200]
-            
+
             with difference:
                 final=[100,0,20,30,50]
-            
+
         """
-        super(UserGrowth_Spaced,self).__init__()
+        super(UserGrowth_Spaced, self).__init__()
 
-                
-        self._initial_users=initial_users
-        self.max_users=max_users
-        self.num_steps=num_steps
-        self.space_function=space_function
-        self._noise_component=noise_addons
-        
-        self.name=name
-        
-        self._num_users_store_original=np.round(self.space_function(start=self._initial_users,
-                                                                    stop=self.max_users,num=num_steps)).astype(int)
-        self._num_users_store=copy.deepcopy(self._num_users_store_original)
+        self._initial_users = initial_users
+        self.max_users = max_users
+        self.num_steps = num_steps
+        self.space_function = space_function
+        self._noise_component = noise_addons
 
-        #applies the noise addon. If a value is below 0, then it is kept at 0.
-        if self._noise_component!=None:
-            dummy=[]
+        self.name = name
+
+        self._num_users_store_original = np.round(
+            self.space_function(
+                start=self._initial_users, stop=self.max_users, num=num_steps
+            )
+        ).astype(int)
+        self._num_users_store = copy.deepcopy(self._num_users_store_original)
+
+        # applies the noise addon. If a value is below 0, then it is kept at 0.
+        if self._noise_component != None:
+            dummy = []
             for i in range(len(self._num_users_store)):
                 temporary = self._num_users_store_original[i]
                 for noiser in self._noise_component:
                     temporary = noiser.apply(value=temporary)
-    
-                if temporary>=0:
+
+                if temporary >= 0:
                     dummy.append(temporary)
                 else:
                     dummy.append(self._num_users_store_original[i])
-                
-            self._num_users_store=dummy
-        #self.num_users=self._initial_users
-        
-        if use_difference:
-            self._num_users_store_original=[self._initial_users]+np.diff(self._num_users_store_original).tolist()
-        
-            self._num_users_store=copy.deepcopy(self._num_users_store_original)
 
-        
-        self.max_iterations=num_steps
-    
-    def execute(self)->int:
-        
-        users=super(UserGrowth_Spaced,self).execute()
-        
+            self._num_users_store = dummy
+        # self.num_users=self._initial_users
+
+        if use_difference:
+            self._num_users_store_original = [self._initial_users] + np.diff(
+                self._num_users_store_original
+            ).tolist()
+
+            self._num_users_store = copy.deepcopy(self._num_users_store_original)
+
+        self.max_iterations = num_steps
+
+    def execute(self) -> int:
+
+        users = super(UserGrowth_Spaced, self).execute()
+
         return users
-    
-    
-    def reset(self)->None:
+
+    def reset(self) -> None:
         """
         recalculates the noise component and sets the user counter back to 0
         """
-        
-        self._num_users_store=copy.deepcopy(self._num_users_store_original)
-        
-        #applies the noise addon. If a value is below 0, then it is kept at 0.
-        if self._noise_component!=None:
-            dummy=[]
+
+        self._num_users_store = copy.deepcopy(self._num_users_store_original)
+
+        # applies the noise addon. If a value is below 0, then it is kept at 0.
+        if self._noise_component != None:
+            dummy = []
             for i in range(len(self._num_users_store)):
                 temporary = self._num_users_store_original[i]
                 for noiser in self._noise_component:
-                    temporary = noiser.apply(**{'value':temporary})
-    
-                if temporary>=0:
+                    temporary = noiser.apply(**{"value": temporary})
+
+                if temporary >= 0:
                     dummy.append(temporary)
                 else:
                     dummy.append(self._num_users_store_original[i])
-        
-            self._num_users_store=dummy
-            
-        self.iteration=0
-        self.num_users=self._initial_users
-        
-        
-    def get_users_store(self)->List:
+
+            self._num_users_store = dummy
+
+        self.iteration = 0
+        self.num_users = self._initial_users
+
+    def get_users_store(self) -> List:
         """
         Returns the list that contains the pre-calculated number of users. This is a
         private variable.
 
         """
-        
+
         return self._num_users_store
-    
-    def get_data(self)->List:
+
+    def get_data(self) -> List:
         """
         Used for compatibility reasons
         """
-        
+
         return self.get_users_store()
-    
+
     def plot_users(self):
-        
+
         plt.plot(self.get_users_store())
-        plt.xlabel('unit of time')
-        plt.ylabel('users')
-    
-    
-    
-        
+        plt.xlabel("unit of time")
+        plt.ylabel("users")
+
+
 class UserGrowth_Constant(UserGrowth):
     """
     Simplest type of usergrowth class. The user growth simply stays constant.
     """
-    
-    def __init__(self,constant:int):
+
+    def __init__(self, constant: int):
         """
-        
+
         Parameters
         ----------
         constant : int
             The total number of users.
 
         """
-        
-        super(UserGrowth_Constant,self).__init__()
-        self.num_users=constant        
-        
-        
+
+        super(UserGrowth_Constant, self).__init__()
+        self.num_users = constant
+
 
 class UserGrowth_FromData(UserGrowth):
-    
-    def __init__(self,data:int):
-        
-        super(UserGrowth_FromData,self).__init__()
-        
+
+    def __init__(self, data: int):
+
+        super(UserGrowth_FromData, self).__init__()
+
         self._num_users_store = np.ndarray.flatten(np.array(data))
-        
-    
-    
-      
+
+
 class UserGrowth_Stochastic(UserGrowth):
     """
     UserGrowth that simply samples a random value during each iteration.
-    
+
     """
-    
-    
-    def __init__(self,user_growth_distribution:scipy.stats=scipy.stats.poisson,
-                 user_growth_dist_parameters:Union[List,Dict]=[{'mu':1000}],add_to_userbase:bool=False,
-                 num_initial_users:int=0,noise_add_ons:[AddOn]=None):
+
+    def __init__(
+        self,
+        user_growth_distribution: scipy.stats = scipy.stats.poisson,
+        user_growth_dist_parameters: Union[List, Dict] = [{"mu": 1000}],
+        add_to_userbase: bool = False,
+        num_initial_users: int = 0,
+        noise_add_ons: [AddOn] = None,
+    ):
         """
-        
+
 
         Parameters
         ----------
         user_growth_distribution : scipy.stats, optional
             The distribution to sample values from. The default is scipy.stats.poisson.
-        
+
         user_growth_dist_parameters : Union[List,Dict], optional
             Either a list of dictionaries, or a dictionary. If a list is provided
             then the parameters change at every iteration. The default is [{'mu':1000}].
-        
-        add_to_user_base : bool, optional 
+
+        add_to_user_base : bool, optional
         If False, then the actual number of users is sampled from the distribution. If True,
         then this number of users is added to the total number of users. Default is false.
-        
+
         num_initial_users : int, optional
         The number of initial users. Default is 0
         """
-        
-        super(UserGrowth_Stochastic,self).__init__()
 
-        
-        self.user_growth_distribution=user_growth_distribution
-        self.user_growth_dist_parameters=user_growth_dist_parameters
-        self.num_users=num_initial_users
-        self.add_to_userbase=add_to_userbase
-        
-        
+        super(UserGrowth_Stochastic, self).__init__()
+
+        self.user_growth_distribution = user_growth_distribution
+        self.user_growth_dist_parameters = user_growth_dist_parameters
+        self.num_users = num_initial_users
+        self.add_to_userbase = add_to_userbase
+
         return None
-    
+
     def execute(self):
-        seed=int(int(time.time())*np.random.rand())
-        if type(self.user_growth_dist_parameters)==list:
+        seed = int(int(time.time()) * np.random.rand())
+        if type(self.user_growth_dist_parameters) == list:
             try:
-                params=self.user_growth_dist_parameters[self.iteration]
+                params = self.user_growth_dist_parameters[self.iteration]
             except:
-                warnings.warn("Iterations for UserGrowth stochastic exhausted. Simply using the last item on the list of parameters.")
-                params=self.user_growth_dist_parameters[-1]
+                warnings.warn(
+                    "Iterations for UserGrowth stochastic exhausted. Simply using the last item on the list of parameters."
+                )
+                params = self.user_growth_dist_parameters[-1]
         else:
-            params=self.user_growth_dist_parameters
-            
-        self._active_params=params
-        value=self.user_growth_distribution.rvs(size=1,random_state=seed,**params)[0]
-        
+            params = self.user_growth_dist_parameters
+
+        self._active_params = params
+        value = self.user_growth_distribution.rvs(size=1, random_state=seed, **params)[
+            0
+        ]
+
         if self.add_to_userbase:
-            self.num_users+=value
+            self.num_users += value
         else:
-            self.num_users=value
-        
-        #apply noise components (if any)
-        if self._noise_component!=None:
+            self.num_users = value
+
+        # apply noise components (if any)
+        if self._noise_component != None:
             value = self.num_users
             for noiser in self._noise_component:
-                value = noiser.apply(**{'value':value})
-                
-            self.num_users=value
-            
-        self.iteration+=1
+                value = noiser.apply(**{"value": value})
+
+            self.num_users = value
+
+        self.iteration += 1
         return self.num_users
-    
-    
